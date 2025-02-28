4b4d8b9b7a1cc5bc82f238d391af2fb7970d00cb
MikeHedde <mickael.hedde@inrae.fr>
2025-02-28 09:51:19 +0100

    gdm version semi definitive

--- analyses/GDM.R
+++ analyses/GDM.R
@@ -1,15 +1,10 @@
-librarian::shelf(ggplot2, dplyr, tidyr, gdm, forcats)
+librarian::shelf(ggplot2, dplyr, tidyr, gdm, forcats, ggpubr)
 
 
 
 #Données environnementales
 habitat <- read.csv("data/derived-data/liste_habitat.csv", h = T, sep = ";") %>%
-  rename_with(toupper) %>% 
-  mutate(
-    HAB = case_when(
-      HABITAT_TYPE=="FERME" ~ 0,
-      HABITAT_TYPE=="OUVERT" ~ 100
-    ))
+  rename_with(toupper)
   
 sol <- read.csv("data/raw-data/pc_sols.csv", h = T, sep = ";") %>%
   select(STATION, PARAM, valeur) %>%
@@ -121,20 +116,17 @@ sumCoef_diplo <- as.data.frame(tapply(gdm.3$coefficients,
   mutate(predictors=gdm.3$predictors, taxa = "diplopoda") %>%
   rename("coef" = 1)
 
-sumCoef <- rbind(sumCoef_coll, sumCoef_iso, sumCoef_diplo)
-
-sumCoef <- sumCoef  %>%
+sumCoef <- rbind(sumCoef_coll, sumCoef_iso, sumCoef_diplo) %>%
   mutate(grp = case_when(predictors == "HAB" ~ "habitat",
-                         predictors %in% c("CN", "Ctot", "COT") ~ "C_resource",
-                         predictors %in% c("N", "Ca", "Mg", "Mn", "K", "Na", "calc", "CEC") ~ "nutrients",
+                         predictors %in% c("CN", "Ctot", "COT", "N", "Ca", "Mg", "Mn", "K", "Na", "calc", "CEC") ~ "nutrients & C",
                          predictors %in% c("DD_cum", "T2MDEW_moy", "T2max_moy", "EVLAND_moy") ~ "climat",
                          predictors %in% c("pH", "Al", "Hum_res") ~ "stress",
                          predictors == "ALTITUDE" ~ "altitude", 
                          predictors == "Geographic" ~ "geographic")) %>%
   mutate(grp = fct_relevel(grp, c("geographic", "habitat", "climat", "altitude", 
-                          "stress", "nutrients", "C_resource")))
+                          "stress", "nutrients & C")))
 
-ggplot(sumCoef, aes(x=taxa, y = coef, fill = grp))+
+sumCoef_p <- ggplot(sumCoef, aes(x=taxa, y = coef, fill = grp))+
   geom_bar(position="stack", stat="identity", colour = "grey")+
   coord_flip()+
   labs(x = "", y = "Percentage of deviance explained", 
@@ -148,10 +140,58 @@ predecol_diplo <- data.frame(taxa = "diplo", obs = gdm.3$observed, ecol = gdm.3$
 
 predecol <- tibble(rbind(predecol_coll, predecol_iso, predecol_diplo))
 
-ggplot(predecol, aes(x = ecol, y = obs))+
+predecol_p <- ggplot(predecol, aes(x = ecol, y = obs))+
   geom_point(aes(colour = taxa, alpha = 0.01))+
   geom_line(aes(x = ecol, y = pred, colour = taxa), lwd=2)+
   facet_wrap(~taxa, scales = "free_x")+
   labs(x = "Predicted ecological distance", y = "Composition dissimilarity")+
   theme_bw()+
   theme(legend.position="none")
+
+# Splines
+sumCoef_wide <- pivot_wider(sumCoef, id_cols = c(predictors, grp), names_from = taxa, values_from = coef)
+
+gdm.1.y <- as.data.frame(isplineExtract(gdm.1)$y) %>%
+  mutate(taxa = "collembola", param = "ecol_dist")
+gdm.1.x <- as.data.frame(isplineExtract(gdm.1)$x)  %>%
+  mutate(taxa = "collembola", param = "predictor")
+gdm.2.y <- as.data.frame(isplineExtract(gdm.2)$y)  %>%
+  mutate(taxa = "isopoda", param = "ecol_dist")
+gdm.2.x <- as.data.frame(isplineExtract(gdm.2)$x)  %>%
+  mutate(taxa = "isopoda", param = "predictor")
+gdm.3.y <- as.data.frame(isplineExtract(gdm.3)$y)  %>%
+  mutate(taxa = "diplopoda", param = "ecol_dist")
+gdm.3.x <- as.data.frame(isplineExtract(gdm.3)$x)  %>%
+  mutate(taxa = "diplopoda", param = "predictor")
+
+param.x <- rbind(gdm.1.x, gdm.2.x, gdm.3.x) %>%
+  rename_at(vars(-taxa, -param), ~ paste0(., '_x'))
+param.y <- rbind(gdm.1.y, gdm.2.y, gdm.3.y) %>%
+  rename_at(vars(-taxa, -param), ~ paste0(., '_y'))
+
+splines_COT <- data.frame(x = param.x$COT_x, 
+                     y = param.y$COT_y, 
+                     taxa = param.x$taxa,
+                     param = "COT")
+
+splines_geo <- data.frame(x = param.x$Geographic_x, 
+                          y = param.y$Geographic_y, 
+                          taxa = param.x$taxa,
+                          param = "geo")
+
+splines_gdm <- rbind(splines_COT, splines_geo)
+
+splines_p <- ggplot(splines_gdm, aes(x = x, y = y, colour = taxa))+
+  geom_line()+
+  labs(y="Partial ecological distance", x="")+
+  facet_wrap(.~param, scales = "free_x",
+             labeller = as_labeller(c(COT = "Tot Organic C\n(mg. g-1)", geo = "Geographic")),
+             strip.position = "bottom")+
+  theme_bw()+
+  theme(strip.background = element_blank(),
+        strip.placement = "outside")
+
+
+ggarrange(sumCoef_p, predecol_p, splines_p, 
+          labels = c("A", "B", "C"),
+          ncol = 2, nrow = 2)
