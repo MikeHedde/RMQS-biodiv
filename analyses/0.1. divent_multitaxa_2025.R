1dc9b516197f8af02d6f827de4ed504607e57dfb
MikeHedde <mickael.hedde@inrae.fr>
2025-09-07 15:56:01 +0200

    SAC propres

--- analyses/0.1. divent_multitaxa_2025.R
+++ analyses/0.1. divent_multitaxa_2025.R
@@ -1,33 +1,45 @@
-librarian::shelf(ggplot2, dplyr, forcats, ade4)
 
+librarian::shelf(dplyr, tidyr, ggplot2, vegan, forcats, divent)
 
 data_car <- read.csv("data/raw-data/lab files/databases update/carabidae_250906.csv", h = T, sep = ";", fileEncoding="latin1")
 data_ara <- read.csv("data/raw-data/lab files/databases update/araneae_250906.csv", h = T, sep = ";", fileEncoding="latin1")
 
+X <- data_car
 
-list_hab <- read.csv("data/derived-data/liste_habitat.csv", h = T, sep = ";") %>%
-  rename_with(toupper) 
+mymat0 <- X %>%
+  #filter(RANG == "ES") %>%
+  group_by(STATION, METHODE, LB_NOM) %>%
+  summarise(abtot = sum(ABONDANCE_TOTALE)) %>%
+  pivot_wider(names_from = LB_NOM, values_from = abtot, values_fill = 0) %>%
+  mutate_if(is.numeric, ~replace_na(., 0))
 
-source("analyses/0. fonctions maison/DIVERSITY.R")
-divent_car_ba <- DIVERSITY(data = data_car[data_car$METHODE == "Pitfall",])
-divent_car_gpd <- DIVERSITY(data = data_car[data_car$METHODE == "GPD",])
-divent_ara_ba <- DIVERSITY(data = data_ara[data_ara$METHODE == "Pitfall",])
-divent_ara_gpd <- DIVERSITY(data = data_ara[data$ara$METHODE == "GPD",])
+# Étape 2. Calcul du taux d'accumulation d'espèces
+abd <- as_abundances(mymat0[,3:ncol(mymat0)])
 
+abd_more_than_1_sp <- abd %>%
+  filter(weight>1)
+
+acc <- accum_hill(
+  abd_more_than_1_sp, 
+  q = 0,
+  n_simulations = 10)  # l'augmentation du nombre de simulations diminue la variabilité de l'enveloppe
 
 # Species accumulation curves
-acc  <- rbind(cbind(taxa = "carabidae", METHOD = "Pitfall", divent_car_ba$acc), 
-              cbind(taxa = "carabidae", METHOD = "GPD", divent_car_gpd$acc),
-              cbind(taxa = "araneae", METHOD = "Pitfall", divent_ara_ba$acc) 
-              #cbind(taxa = "araneae", METHOD = "GPD", divent_ara_gpd$acc)
-              ) %>%
-  left_join(divent_car_ba$station) 
+site_names <- tibble("site" = abd$site,
+      "methode" = mymat0$METHODE,
+      "station" = mymat0$STATION)
+
+acc <- left_join(acc, site_names) %>%
+  filter(estimator == "Interpolation")
 
       # représentation
-      ggplot(acc, aes(x = level, y = diversity, group = STATION)) +
-        geom_path(aes(colour = STATION))+
-        facet_grid(METHOD ~ taxa)+
-        theme_bw()
+      ggplot(acc, aes(x = level, y = diversity, group = station)) +
+        geom_path(aes(colour = station))+
+        labs(x="Nb of individuals", y = "Species Number", title =  "Carabidae")+
+        facet_grid(methode ~ .)+
+        lims(y=c(0,20))+
+        theme_bw()+
+        guides(colour="none")
       
 
   
