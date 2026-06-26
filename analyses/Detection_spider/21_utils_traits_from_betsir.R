efb1ec9ec0b1861ce3930faf9bee7a659cc2dc85
Hedde <122521134+MikeHedde@users.noreply.github.com>
2026-06-26 11:02:56 +0200

    delete obsolete scripts and figures

--- analyses/Detection_spider/21_utils_traits_from_betsir.R
+++
@@ -1,4 +0,0 @@
-# Utilitaire si tu veux brancher BETSI plus tard
-# devtools::install_git("https://src.koda.cnrs.fr/cefe/bioflux/betsir.git")
-# library(betsir)
-# h <- get_coded_value("Carabidae", "diet", include_subtaxa = TRUE)
